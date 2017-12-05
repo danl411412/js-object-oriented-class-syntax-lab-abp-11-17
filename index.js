a6d5326cef27a8e6d7548376b7ebd4b6c4b96937
danl411412 <daan.b.lee@gmail.com>
2017-12-05 00:35:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ class BoardMember{
     this.training = training;
   }
 
-  this.veto = function() {
+  veto() {
     return "No, I must disagree";
   }
 
-  this.approve = function() {
+  approve() {
     return "You can do that!";
   }
 
-  this.doCharity = function() {
+  doCharity() {
     return "I like to help people";
   }
 
-  this.releasePressStatement = function() {
+  releasePressStatement() {
     return "You will see great things from Scruber.";
   }
 
-  this.sayHi = function() {
+  sayHi() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
   }
 }
